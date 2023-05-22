d78efd10699531413bff64fe137c15e58fae76a7
Karthik <karthik5112001@gmail.com>
2023-05-23 00:04:16 +0530

    commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { AiFillInstagram } from 'react-icons/ai'
+import { FaTwitter } from 'react-icons/fa'
+import { TfiLinkedin } from 'react-icons/tfi'
+import { BsFacebook } from 'react-icons/bs'
 
 import './style.css'
 
@@ -7,20 +11,72 @@ const SUIFooter = () => {
         <div>
             <div className="SUIFooter">
                 <div className="SUIFooterContainer">
-                    <div className="SUIFooterNewsLetterContainer">
-
-                    </div>
-                    <div className="SUIFooterLogoContainer">
-                        <img src="https://firebasestorage.googleapis.com/v0/b/skynoveau-office.appspot.com/o/logo.png?alt=media&token=e375539c-5dff-44c8-98b2-50b8adc60488" />
-                        <div className="SUIFooterDescriptionContainer">
-                            <p>The Wix website builder offers a complete solution from enterprise-grade infrastructure and business features to advanced SEO and marketing tools–enabling anyone to create and grow online.</p>
-                        </div>
-                        <div className="SUIFooterSocialmediaContainer">
-
+                <div className="SUIFooterNewsLetterContainer">
+                        <p>Subscribe our news letter</p>
+                        <div className="SUIFooterNewsLetterInputContainer">
+                            <input />
+                            <button>Submit</button>
                         </div>
                     </div>
-                    <div className="SUIFooterLinkContainer">
-                        
+                    <div className="SUIFooterBottomContainer"> 
+                        <div className="SUIFooterLogoContainer">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/skynoveau-office.appspot.com/o/logo.png?alt=media&token=e375539c-5dff-44c8-98b2-50b8adc60488" />
+                            <div className="SUIFooterDescriptionContainer">
+                                <p>The Wix website builder offers a complete solution from enterprise-grade infrastructure</p>
+                            </div>
+                            <div className="SUIFooterSocialmediaContainer">
+                                <div className='socialMediaIcon'>
+                                    <a href='https://www.instagram.com/woodheadcreative_official/'>
+                                        <AiFillInstagram />
+                                    </a>
+                                </div>
+                                <div className='socialMediaIcon'>
+                                    <a href='https://twitter.com/WoodheadWhc'>
+                                        <FaTwitter />
+                                    </a>
+                                </div>
+                                <div className='socialMediaIcon'>
+                                    <a href='https://www.linkedin.com/company/woodhead-creative/'>
+                                        <TfiLinkedin />
+                                    </a>
+                                </div>
+                                <div className='socialMediaIcon'>
+                                    <a href='https://www.facebook.com/WoodHeadCreative'>
+                                        <BsFacebook />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="SUIFooterLinkContainer">
+                            <div className="SUIFooterLinkContainerBox">
+                                <div className="SUIFooterLinkContainerBoxItem">
+                                    <ul>
+                                        <li>QUICK LINKS</li>
+                                        <li>Home</li>
+                                        <li>Services</li>
+                                        <li>Clients</li>
+                                        <li>Testimonials</li>
+                                    </ul>
+                                </div>
+                                <div className="SUIFooterLinkContainerBoxItem">
+                                    <ul>
+                                        <li>QUICK CONTACT</li>
+                                        <li>Feel free to contact</li>
+                                        <li>+91-9840717144</li>
+                                        <li>woodheadcreative@gmail.com</li>
+                                    </ul>
+                                </div>
+                                <div className="SUIFooterLinkContainerBoxItem">
+                                    <ul>
+                                        <li>OUR ADDRESS</li>
+                                        <li>WoodHeadCreative</li>
+                                        <li>D125/E, Santhosh Colony, K.K</li>
+                                        <li>Nagar,Chennai - 600078</li>
+                                    </ul>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
