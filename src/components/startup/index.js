b4c31bad5b6c2d5c0e00b0fa1695315b6e2069ea
karthik B <82690268+karthik-noveau@users.noreply.github.com>
2023-06-13 13:56:39 +0000

    commit

--- src/components/startup/index.js
+++ src/components/startup/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+
+import { SUIRippleButton } from '../../SUI_Library_Components/widgets/buttons/ripplebutton'
 import './style.css'
 
 function StartUp() {
     return (
-        <div>StartUp</div>
+        <div className='startUp'>
+            <div className='startUpBanner'>
+                
+            </div>
+            <div className='startUpText'>
+                <div className='startUpTextContainer'>
+                    <p>SKY UI 1.0</p>
+                    <p>Help designers/developers building beautiful products more flexible and working with happiness</p>
+                    <SUIRippleButton className="startUpButton">
+                        Getting Started
+                    </SUIRippleButton>
+                </div>
+            </div>
+        </div>
     )
 }
 
