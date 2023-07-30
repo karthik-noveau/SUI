12b02a306416d746b9894463ba3a08def9f41926
karthik <karthik5112001@gmail.com>
2023-07-30 23:43:08 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const URL = "http://sui.skynoveau.in";
-export const URL = "http://localhost:3000";
+export const URL = "http://sui.skynoveau.in";
+// export const URL = "http://localhost:3000";
